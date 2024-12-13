18af5a9015b0693d6d485c40497d9790004ed53d
Michael Mosquera <michael2000ny@gmail.com>
2024-12-12 19:16:01 -0500

    imported bootstrap to main

--- React-Portfolio/src/main.jsx
+++ React-Portfolio/src/main.jsx
@@ -1,5 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
