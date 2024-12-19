9b7f43a7a52e5c4c54622ee3a70fb4cc5da69216
Michael Mosquera <michael2000ny@gmail.com>
2024-12-18 19:54:29 -0500

    bootsrap js and navbar

--- React-Portfolio/src/main.jsx
+++ React-Portfolio/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Includes Popper.js
 import './styles/index.css';
 import App from './App.jsx'
 
