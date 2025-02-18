7dac45acae1f4964d10004aa7f644df5dbf3be8d
Michael Mosquera <michael2000ny@gmail.com>
2025-02-18 16:53:57 -0500

    fixed image srcs

--- React-Portfolio/src/pages/Portfolio.jsx
+++ React-Portfolio/src/pages/Portfolio.jsx
@@ -5,8 +5,8 @@ const Portfolio = () => {
     <section className="container">
       <h1>Portfolio</h1>
       <div className="row">
-        <Project title="Pet Tag" image="../Assets/Images/PetTagMain.png" deploy="https://achensen.github.io/Pet-Tag/index.html" repo="https://github.com/achensen/Pet-Tag" />
-        <Project title="Project 2" image="../Assets/Images/NewFlowMainPage.png" deploy="#" repo="https://github.com/achensen/New-Flow" />
+        <Project title="Pet Tag" image="/PetTagMain.png" deploy="https://achensen.github.io/Pet-Tag/index.html" repo="https://github.com/achensen/Pet-Tag" />
+        <Project title="Project 2" image="/NewFlowMainPage.png" deploy="#" repo="https://github.com/achensen/New-Flow" />
         <Project title="Project 3" image="https://via.placeholder.com/300" deploy="#" repo="#" />
       </div>
     </section>
